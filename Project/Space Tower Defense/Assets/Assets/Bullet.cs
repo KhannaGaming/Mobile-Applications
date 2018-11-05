8dd6226642cf54f59ae3772144dcc324c08aa04d
EliteSalad <xavieroliver42@gmail.com>
2018-11-05 14:43:23 +0000

    All Turrets Imported
    Turrets imported named ready to be animated and have behaviors added

---
+++ Project/Space Tower Defense/Assets/Assets/Bullet.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    public Transform target;
+    public float speed = 10f;
+    public GameObject impactEffect;
+    public void Chase(Transform _target)
+    {
+        //instantiate sposion effect
+        target = _target;
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (target == null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Vector3 dir = target.position - transform.position;
+        float distanceThisFrame = speed * Time.deltaTime;
+
+        if(dir.magnitude <= distanceThisFrame)
+        {
+            HitTarget();
+            return;
+        }
+
+        transform.Translate(dir.normalized * distanceThisFrame, Space.World);
+	}
+
+    void HitTarget()
+    {
+
+        GameObject effectIns = (GameObject)Instantiate(impactEffect, transform.position, transform.rotation);
+        Destroy(effectIns, 2f);
+
+        //Destroy(target.gameObject);
+
+        Destroy(gameObject);
+        //Debug.Log("Hit");
+    }
+
+
+}
---
+++ Project/Space Tower Defense/Assets/Assets/Imported/Turrets/Sci Fi Assets/Simple SciFi Gun Turret Set/Scripts/SimpleObjectRotateC.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleObjectRotateC : MonoBehaviour {
+
+    public float rotationSpeed = 100f;
+    public Vector3 direction = new Vector3(0, 0, 1);
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(direction * rotationSpeed * Time.deltaTime);
+	}
+}
