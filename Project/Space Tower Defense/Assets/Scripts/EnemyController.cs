12645b01df7757225d315ef2db303f3c6b1904e9
Sean Khanna <sean6beechdrive@hotmail.co.uk>
2018-11-06 14:51:33 +0000

    Level01

---
+++ Project/Space Tower Defense/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour {
+
+    public NavMeshAgent NMA;
+    public GameObject EndPath;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log("num children:"+EndPath.transform.childCount);
+        int childNumber = Random.Range(0, 3);
+        Debug.Log(childNumber);
+       NMA.SetDestination(EndPath.transform.GetChild(childNumber).transform.position);
+	}
+}
