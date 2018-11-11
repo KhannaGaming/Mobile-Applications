59674a3ba55636371604e04270ed7a0268ca3336
Sean Khanna <sean6beechdrive@hotmail.co.uk>
2018-11-11 18:21:23 +0000

    Template Level01 done

--- Project/Space Tower Defense/Assets/Scripts/EnemyController.cs
+++ Project/Space Tower Defense/Assets/Scripts/EnemyController.cs
@@ -5,19 +5,53 @@ using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour {
 
-    public NavMeshAgent NMA;
+    public  NavMeshAgent NMA;
     public GameObject EndPath;
+    public int currentPathNode = 1;
+    public int childNumber = 0;
+    private int health = 10;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+        // Use this for initialization
+    void Start ()
+    {
+         childNumber = Random.Range(0, 3);
+        EndPath = GameObject.Find("PathWNodes (" + currentPathNode + ")");
+        NMA.SetDestination(EndPath.transform.GetChild(childNumber).transform.position);
+    }
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
         Debug.Log("num children:"+EndPath.transform.childCount);
-        int childNumber = Random.Range(0, 3);
+         childNumber = Random.Range(0, 3);
         Debug.Log(childNumber);
-       NMA.SetDestination(EndPath.transform.GetChild(childNumber).transform.position);
+
+        if (Vector3.Distance(transform.position, NMA.destination) <= 0.5f)
+        {
+            currentPathNode++;
+            EndPath = GameObject.Find("PathWNodes (" + currentPathNode + ")");
+            if (EndPath)
+            {
+                NMA.SetDestination(EndPath.transform.GetChild(childNumber).transform.position);
+            }
+            else
+            {
+                GameObject.Find("GameManager").GetComponent<GameController>().reduceHealth();
+                Destroy(this.gameObject);
+                
+                                
+            }
+        }
 	}
+
+    ///   <summary>
+    ///   Decreases health by input amount.
+    ///   </summary>
+    ///   <param name="damage">Amount to decrease health by.
+    ///   </param>
+    void damageHealth(int damage)
+    {
+        health -= damage;
+    }
+ 
 }
---
+++ Project/Space Tower Defense/Assets/Scripts/GameController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public int Health = 10;
+    public GameObject EnemyPrefab;
+    public Transform EnemySpawnLocation;
+	// Use this for initialization
+	void Start () {
+        for (int i = 0; i < 5; i++)
+        {
+            Instantiate(EnemyPrefab, EnemySpawnLocation.position, EnemySpawnLocation.rotation);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void reduceHealth()
+    {
+        Health--;
+    }
+}
