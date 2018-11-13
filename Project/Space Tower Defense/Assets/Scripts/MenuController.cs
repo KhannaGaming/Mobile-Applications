dbdd3ddc446c1cb5e64916c03ccf641a550de402
Jamie Utting <JLUtting97@gmail.com>
2018-11-13 14:44:41 +0000

    Updates
    Buttons link scenes and visual update.

---
+++ Project/Space Tower Defense/Assets/Scripts/MenuController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+
+    // Used for loading scenes using buttons
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    // Used for the exit game button
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
