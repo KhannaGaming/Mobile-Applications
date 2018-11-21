f2dec3a3d4986895499116cb4eb80a0e71a59726
Jamie Utting <JLUtting97@gmail.com>
2018-11-21 16:08:56 +0000

    UI Updated
    UI Updated

---
+++ Project/Space Tower Defense/Assets/Scripts/WaveController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaveController : MonoBehaviour
+{
+
+    // Variables
+    public static int waveValue = 0; // Used to keep track of the amount of waves the player has completed
+    Text waves; // Variable needed to make a connection to the wave counter text object
+
+    // Use this for initialization
+    void Start()
+    {
+        // Makes a reference to the wave counter text object
+        waves = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Sets the text value to "Waves Completed: waveValue variable"
+        waves.text = "Waves Completed: " + waveValue;
+    }
+}
