5cc284041716b13c5c83fe867eebdddad1feaa10
Sean Khanna <sean6beechdrive@hotmail.co.uk>
2018-11-21 12:16:43 +0000

    ps

---
+++ Project/Space Tower Defense/Assets/particleSystemEditor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class particleSystemEditor : MonoBehaviour {
+
+    private ParticleSystem ps;
+
+	// Use this for initialization
+	void Start () {
+        ps = GetComponent<ParticleSystem>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(transform.parent.GetChild(1).name  == "Turret 1a")
+        {
+            ParticleSystem.MainModule main = ps.main;
+            main.startColor =  Color.red;
+                    }
+	}
+}
