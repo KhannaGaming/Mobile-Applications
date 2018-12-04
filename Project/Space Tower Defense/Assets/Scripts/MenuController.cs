b3a87312814d22dfb99d6b1cc1acde60f2732422
Jamie Utting <JLUtting97@gmail.com>
2018-12-04 15:06:01 +0000

    Music
    Working on mute button

--- Project/Space Tower Defense/Assets/Scripts/MenuController.cs
+++ Project/Space Tower Defense/Assets/Scripts/MenuController.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
 public class MenuController : MonoBehaviour
 {
+    GameObject MusicToggle;
+     bool musicToggle;
 
     // Used for loading scenes using buttons
     // Takes a string parameter
@@ -17,5 +19,32 @@ public class MenuController : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    private void Start()
+    {
+        MusicToggle = GameObject.Find("Music On/Off");
+
+        if (PlayerPrefs.GetInt("MusicToggle", 1) == 1)
+        {
+            musicToggle = true;
+        }
+        else
+        {
+            musicToggle = false;
+        }
+
+        MusicToggle.GetComponent<Toggle>().isOn = musicToggle;
+
+    }
+    private void Update()
+    {
+        MusicToggle.GetComponent<Toggle>().isOn = musicToggle;
+
+    }
+    public void toggleMusic()
+    {
+        
+        MusicToggle.GetComponent<Toggle>().isOn = !MusicToggle.GetComponent<Toggle>().isOn;
     }
 }
--- Project/Space Tower Defense/Assets/Scripts/MusicController.cs
+++ Project/Space Tower Defense/Assets/Scripts/MusicController.cs
@@ -4,9 +4,45 @@ using UnityEngine;
 
 public class MusicController : MonoBehaviour
 {
-    void Awake()
+
+    public bool mute = false;
+    public static float volume;
+
+    private AudioSource AS;
+
+    public GameObject MusicToggleButton;
+    void Start()
     {
-        // Makes sure the game object is not deleted when the user moves to a new scene
-        DontDestroyOnLoad(transform.gameObject);
+        //// Makes sure the game object is not deleted when the user moves to a new scene
+        //DontDestroyOnLoad(transform.gameObject);
+        AS = this.GetComponent<AudioSource>();
+        AS.volume = PlayerPrefs.GetFloat("MusicVolume", 1.0f);
+        PlayerPrefs.GetInt("MusicToggle", 1);
+    }
+
+    public void muteMusic()
+    {
+        if(PlayerPrefs.GetFloat("MusicVolume",1.0f)==1.0f)
+        {
+            PlayerPrefs.SetFloat("MusicVolume", 0.0f);
+            AS.volume = 0.0f;
+            PlayerPrefs.SetInt("MusicToggle", 0);
+            MusicToggleButton.GetComponent<MenuController>().toggleMusic();
+        }
+        else if (PlayerPrefs.GetFloat("MusicVolume", 1.0f) == 0.0f)
+        {
+            PlayerPrefs.SetFloat("MusicVolume", 1.0f);
+            AS.volume = 1.0f;
+            PlayerPrefs.SetInt("MusicToggle", 1);
+            MusicToggleButton.GetComponent<MenuController>().toggleMusic();
+        }
+        //if (mute == false)
+        //{
+        //    AudioListener.volume = 1.0f;
+        //}
+        //else if (mute == true)
+        //{
+        //    AudioListener.volume = 0.0f;
+        //}
     }
 }
