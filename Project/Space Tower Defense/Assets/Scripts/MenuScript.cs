df3ed8ba3ea1cf8025cf7ef7c718d00c92d0ac7c
Jamie Utting <JLUtting97@gmail.com>
2018-12-05 16:20:00 +0000

    Menu
    Menus updated.

---
+++ Project/Space Tower Defense/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour {
+
+    public List<GameObject> MenuItems;
+
+    public void Open(string Menu_Item_Name)
+    {
+        Debug.Log(Menu_Item_Name);
+        foreach (GameObject item in MenuItems)
+        {
+            if (item.name == Menu_Item_Name)
+            {
+                item.SetActive(true);
+            }
+            else
+                item.SetActive(false);
+        }
+    }
+
+    public void LoadLevel(string level)
+    {
+        SceneManager.LoadScene(level);
+    }
+}
--- Project/Space Tower Defense/Assets/Scripts/MusicController.cs
+++ Project/Space Tower Defense/Assets/Scripts/MusicController.cs
@@ -4,45 +4,15 @@ using UnityEngine;
 
 public class MusicController : MonoBehaviour
 {
-
-    public bool mute = false;
-    public static float volume;
-
     private AudioSource AS;
 
-    public GameObject MusicToggleButton;
-    void Start()
+    private void Awake()
     {
-        //// Makes sure the game object is not deleted when the user moves to a new scene
-        //DontDestroyOnLoad(transform.gameObject);
-        AS = this.GetComponent<AudioSource>();
-        AS.volume = PlayerPrefs.GetFloat("MusicVolume", 1.0f);
-        PlayerPrefs.GetInt("MusicToggle", 1);
+        AS = GetComponent<AudioSource>();
     }
 
-    public void muteMusic()
+    public void Toggle()
     {
-        if(PlayerPrefs.GetFloat("MusicVolume",1.0f)==1.0f)
-        {
-            PlayerPrefs.SetFloat("MusicVolume", 0.0f);
-            AS.volume = 0.0f;
-            PlayerPrefs.SetInt("MusicToggle", 0);
-            MusicToggleButton.GetComponent<MenuController>().toggleMusic();
-        }
-        else if (PlayerPrefs.GetFloat("MusicVolume", 1.0f) == 0.0f)
-        {
-            PlayerPrefs.SetFloat("MusicVolume", 1.0f);
-            AS.volume = 1.0f;
-            PlayerPrefs.SetInt("MusicToggle", 1);
-            MusicToggleButton.GetComponent<MenuController>().toggleMusic();
-        }
-        //if (mute == false)
-        //{
-        //    AudioListener.volume = 1.0f;
-        //}
-        //else if (mute == true)
-        //{
-        //    AudioListener.volume = 0.0f;
-        //}
+        AS.enabled = !AS.enabled;
     }
 }
